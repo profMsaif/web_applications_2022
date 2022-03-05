5e045c20e85a5f99c6136a2e10aeee498a9fa7d8
Jarno Matarmaa <jarno.matarmaa@tuni.fi>
2022-03-05 17:05:03 +0500

    Labwork: Login and Password reset functionality added

--- django/matarmaa/mysite/mysite/blog/urls.py
+++ django/matarmaa/mysite/mysite/blog/urls.py
@@ -2,9 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'), 
+    path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('register', views.register_request, name="register"),
     path('login', views.login_request, name='login'),
-    path("logout", views.logout_request, name= "logout"),
+    path("logout", views.logout_request, name='logout'),
+    path("password_reset", views.password_reset_request, name="password_reset")
 ] 
--- django/matarmaa/mysite/mysite/blog/views.py
+++ django/matarmaa/mysite/mysite/blog/views.py
@@ -1,10 +1,19 @@
-from django.views import generic
 from .models import Post
-from django.shortcuts import  render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login, authenticate, logout
+from django.views import generic
+from django.shortcuts import  render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.models import User
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -51,5 +60,34 @@ def login_request(request):
 def logout_request(request):
 	logout(request)
 	messages.info(request, "You have successfully logged out.") 
-	return redirect("main:homepage")
+	return redirect("home")
 
+
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "password/password_reset_email.txt"
+					c = {
+					"email": user.email,
+					'domain': '127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done")
+
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="password/password_reset.html", context={"password_reset_form":password_reset_form})
