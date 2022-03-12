15f013341009e0ecbf25fb1c1e1730f81bbc6e16
yasserelsayed190@gmail.com <yasserelsayed190@gmail.com>
2022-03-12 16:01:25 +0500

    init

--- django/badr/myblog/app/blog/urls.py
+++ django/badr/myblog/app/blog/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path("logout", views.logout_request, name="logout")
 ]
