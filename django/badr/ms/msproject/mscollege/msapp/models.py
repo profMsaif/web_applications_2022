72c75c42b838cb7049040b4d73717f0095b23b58
yasserelsayed190@gmail.com <yasserelsayed190@gmail.com>
2022-03-27 12:53:16 +0500

    admin,staff

--- django/badr/ms/msproject/mscollege/msapp/models.py
+++ django/badr/ms/msproject/mscollege/msapp/models.py
@@ -132,7 +132,16 @@ class LeaveReportStudent(models.Model):
     leave_status = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = models.Manager()      
+    objects = models.Manager()     
+
+class FeedBackStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager() 
 
 
 
